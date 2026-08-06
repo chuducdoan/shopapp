4402a8841fd64abbf779e376c5c46d550d495501
chuducdoan97 <doancd@vnpay.vn>
2026-08-06 15:47:49 +0700

    add class

---
+++ src/main/java/com/product/shoppapp/controllers/UserController.java
@@ -0,0 +1,44 @@
+package com.product.shoppapp.controllers;
+
+import com.product.shoppapp.dtos.UserDTO;
+import com.product.shoppapp.dtos.UserLoginDTO;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/users")
+public class UserController {
+
+    @PostMapping("/register")
+    public ResponseEntity<?> createUser(@Valid @RequestBody UserDTO userDTO, BindingResult result) {
+       try {
+           if (result.hasErrors()) {
+               List<String> errorMessages = result.getFieldErrors().stream().map(FieldError::getDefaultMessage).toList();
+               return ResponseEntity.badRequest().body(errorMessages);
+           }
+           if (!userDTO.getPassword().equals(userDTO.getRetypePassword())) {
+               return ResponseEntity.badRequest().body("Passwords do not match");
+           }
+           return ResponseEntity.ok("Create user " + userDTO);
+       } catch (Exception e) {
+           return ResponseEntity.badRequest().body("Error: " + e.getMessage());
+       }
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> createUser(@Valid @RequestBody UserLoginDTO userDTO) {
+        try {
+            return ResponseEntity.ok("Create user " + userDTO);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body("Error: " + e.getMessage());
+        }
+    }
+}
